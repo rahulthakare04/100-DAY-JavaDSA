bb0bd4641dd0250387bcf5a730b9f1ff5e385e03
Rahul Thakare <rt140547@gmail.com>
2025-06-02 11:39:47 +0530

    Add Fibonacci code

---
+++ FibonacciNumbers.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0){
+            return 0;
+        }else if(n==1){
+            return 1;
+        }
+        int FristTerm=0;
+        int SecondTerm=1;
+
+        for(int i=1;i<=n;i++){
+            int ThirdTerm=FristTerm+SecondTerm;
+            FristTerm=SecondTerm;
+            SecondTerm=ThirdTerm;
+
+        }
+        return FristTerm;
+        
+    }
+}
