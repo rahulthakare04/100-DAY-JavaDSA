fbbc2d04a646fdf29fb1f8e3c3f38064cfc33abe
rahulthakare04 <rt140547@gmail.com>
2025-06-17 11:23:45 +0530

    day 4 loops que

---
+++ Loops/Que1.java
@@ -0,0 +1,17 @@
+//print rahul thakare n times take n as input form user
+
+import java.util.Scanner;
+
+public class Que1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number : ");
+        int n =sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            System.out.println("rahul thakare ");
+        }
+
+        
+    }
+}
