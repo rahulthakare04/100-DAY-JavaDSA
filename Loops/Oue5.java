283a6da887fa14be204021db4ab67fc44cfb4fd2
rahulthakare04 <rt140547@gmail.com>
2025-06-17 17:05:43 +0530

    day 4

---
+++ Loops/Oue5.java
@@ -0,0 +1,10 @@
+//print all the number 1 to 100 that are divisible by 3
+
+public class Oue5 {
+    public static void main(String[] args) {
+        for(int i=3; i<=100;i++){
+        if(i % 3 == 0)
+        System.out.println(i);
+    }
+    }
+}
---
+++ Loops/Que4.java
@@ -0,0 +1,23 @@
+//print even number 1 to 100 
+
+
+
+public class Que4 {
+    public static void main(String[] args) {
+      
+        // for(int i=2;i<=100;i=i+2){
+        //     System.out.println(i);
+        // }
+
+
+
+
+
+        for(int i=2;i<=100;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
